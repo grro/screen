cdca7712de683ca7ad3914e80fe250d559708fb1
grro <gregror.roth@1und1.de>
2026-03-15 09:39:15 +0100

    wake up improved

--- screen.py
+++ screen.py
@@ -62,48 +62,42 @@ class Screen:
 
     def activate(self):
         self.screen_on_target_state = True
+        with self._lock:
 
-        try:
-            logging.info("enter activate")
-            with self._lock:
+            # bowser
+            if not self._is_browser_running():
+                now = datetime.now()
+                if now > self.last_browser_attempt + timedelta(seconds=15):
+                    self.last_browser_attempt = now
+                    self._start_browser_script()
+                    sleep(2)  # Kurze Wartezeit, damit der Browser initialisiert wird
 
-                # bowser
-                if not self._is_browser_running():
-                    now = datetime.now()
-                    if now > self.last_browser_attempt + timedelta(seconds=15):
-                        self.last_browser_attempt = now
-                        self._start_browser_script()
-                        sleep(2)  # Kurze Wartezeit, damit der Browser initialisiert wird
-
-                # screen power
-                if not self._is_screen_power_on():
-                    if self._set_power_on():
-                        self._notify_listeners()
-                    else:
-                        logging.error("hardware not ready.")
-        finally:
-            logging.info("exit activate")
+            # screen power
+            if not self._is_screen_power_on():
+                if self._set_power_on():
+                    self._notify_listeners()
+                else:
+                    logging.error("hardware not ready.")
 
 
     def deactivate(self):
         self.screen_on_target_state = False
-        try:
-            logging.info("enter deactivate")
-            with self._lock:
-                logging.info("Action: Screen OFF")
-                self._set_power_off()
-                self._stop_browser_script()
-                self._notify_listeners()
-        finally:
-            logging.info("exit deactivate")
+        with self._lock:
+            logging.info("Action: Screen OFF")
+            self._set_power_off()
+            self._stop_browser_script()
+            self._notify_listeners()
+
+
 
     def _set_power_on(self) -> bool:
         output = "HDMI-A-2"
         env = self._get_env()
 
-        logging.info(f"Wecke Hardware {output}...")
         # Schritt 1: Nur Einschalten (Handshake triggern)
-        subprocess.run(["wlr-randr", "--output", output, "--on"], env=env)
+        logging.info(f"Wecke Hardware {output}...")
+        # SNur Einschalten (Handshake triggern)
+        subprocess.run(["wlr-randr", "--output", output, "--on"], env=env, capture_output=True)
 
         # Schritt 2: Dem Monitor Zeit geben (EDID-Aushandlung)
         time.sleep(3)
@@ -169,30 +163,36 @@ class Screen:
 
     def _repair_loop(self):
         while True:
-            time.sleep(20)
+            time.sleep(9)
             if not self._initialized:
                 continue
 
-            self._repair_browser()
-            sleep(5)
+            if self._repair_browser():
+                sleep(3)
             self._repair_screen_power()
 
 
-    def _repair_browser(self):
+    def _repair_browser(self) -> bool:
         try:
             if self.screen_on_target_state and not self._is_browser_running():
                 logging.warning("Repair: Browser nicht aktiv, aber Bildschirm soll an sein.")
                 self._start_browser_script()
+            else:
+                return False
         except Exception as e:
             logging.error(f"Fehler im Browser-Repair: {e}")
+        return True
 
 
-    def _repair_screen_power(self):
+    def _repair_screen_power(self) -> bool:
         try:
             hw_is_on = self._is_screen_power_on()
             if hw_is_on != self.screen_on_target_state:
                 logging.warning(f"Repair: HW ist {hw_is_on}, Soll ist {self.screen_on_target_state}")
                 self._set_power_on() if self.screen_on_target_state else self._set_power_off()
+            else:
+                return False
         except Exception as e:
             logging.error(f"Fehler im Repair: {e}")
+        return True
 
